8700bdc71f5fee75440877e6bdaac964b32091f2
Zaltu <swwouf@hotmail.com>
2018-11-09 17:33:13 -0500

    removed old key

--- harmony.py
+++ harmony.py
@@ -72,5 +72,5 @@ class Harmony(discord.Client):
 
 
 if __name__ == "__main__":
-    H = Harmony('MjU2MTU0OTQ4NzM0MTU2ODAx.DsdCuA.w-SATBWOPKbzhHU8D8275rqP34c')
+    H = Harmony('')
     H.activate()
