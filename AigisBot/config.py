cbf972a3c5fe01d772c9b5656f794723056fe662
Zaltu <swwouf@hotmail.com>
2019-09-09 20:29:50 -0400

    updated config

--- AigisBot/config.py
+++ AigisBot/config.py
@@ -3,9 +3,9 @@ Sample test config for AigisConfig type
 """
 PLUGIN_TYPE = "external"
 ENTRYPOINT = "{root}"
-LAUNCH = "python3.6 {root}/src/main.py"
+LAUNCH = ["python36", "{root}/src/main.py"]
 
-SYSTEM_REQUIREMENTS = ["python3.6", "pip3.6"]
+SYSTEM_REQUIREMENTS = ["python36", "pip3.6"]
 
 REQUIREMENT_COMMAND = "pip3.6 install -r"
 REQUIREMENT_FILE = "{root}/requirements.txt"
