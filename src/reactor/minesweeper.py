3f30f08d66cc1c76b772d6d55fd0e858e8df1d06
Zaltu <swwouf@hotmail.com>
2020-02-06 17:50:45 -0500

    formatting on the minesweeper field

--- src/reactor/minesweeper.py
+++ src/reactor/minesweeper.py
@@ -5,16 +5,16 @@ Simple example of generating a minesweeper field.
 from random import randint
 
 DISCORD_MAP = {
-    0: "|| :zero: ||",
-    1: "|| :one: ||",
-    2: "|| :two: ||",
-    3: "|| :three: ||",
-    4: "|| :four: ||",
-    5: "|| :five: ||",
-    6: "|| :six: ||",
-    7: "|| :seven: ||",
-    8: "|| :eight: ||",
-    "B": "|| :boom: ||"
+    0: "||:zero:||",
+    1: "||:one:||",
+    2: "||:two:||",
+    3: "||:three:||",
+    4: "||:four:||",
+    5: "||:five:||",
+    6: "||:six:||",
+    7: "||:seven:||",
+    8: "||:eight:||",
+    "B": "||:boom:||"
 }
 
 
