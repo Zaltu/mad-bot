291c288bc6611724e901f8ca73d393de6da082f4
Zaltu <swwouf@hotmail.com>
2020-02-06 16:04:14 -0500

    updated furi for new translation_utils

--- src/reactor/actions.py
+++ src/reactor/actions.py
@@ -284,7 +284,7 @@ class Reactor():
         Convert japanese text to furigana (for educational purposes).
         """
         self.post(
-            aigis.japanese(self.delta["text"]).furigana
+            aigis.japanese.to_furigana(self.delta["text"])
         )
 
     def minesweeper(self):
